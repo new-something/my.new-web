36528e59b251ea9bbffa0973e8d3923f43b3cd5c
ndgndg91 <ndgndg91@gmail.com>
2020-11-26 23:13:10 +0900

    <refactor> divide landing component to multiple sub components

---
+++ src/app/components/landing/why-we-built/why-we-built.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-why-we-built',
+  templateUrl: './why-we-built.component.html',
+  styleUrls: ['./why-we-built.component.css']
+})
+export class WhyWeBuiltComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/landing/you-can-do/you-can-do.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-you-can-do',
+  templateUrl: './you-can-do.component.html',
+  styleUrls: ['./you-can-do.component.css']
+})
+export class YouCanDoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
