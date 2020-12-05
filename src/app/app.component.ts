5301befea0b47c7a2fdba7e49f8da874c5e28ec0
ndgndg91 <ndgndg91@gmail.com>
2020-12-05 21:24:56 +0900

    <feat> environment log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, isDevMode, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-new-web';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    if (isDevMode()){
+      console.log('Dev Mode');
+    } else {
+      console.log('Prod Mode');
+    }
+  }
 }
