65703a879ffac98fa4dd44bcfaa0f5b7f35c324a
ndgndg91 <ndgndg91@gmail.com>
2021-01-09 16:31:35 +0900

    <chore> update prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   // 실제 배포 필요 13.125.238.130 임시로 설정
   production: true,
   router: 'https://master.d36hup6pow69e1.amplifyapp.com',
-  userService: 'http://13.125.238.130:8080',
+  userService: 'http://54.180.207.58:8080',
   appService: 'http://13.125.238.130:9090'
 };
