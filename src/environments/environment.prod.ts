aec27db2b4b497af2d1a0b2c7f8448a7b923cd11
ndgndg91 <ndgndg91@gmail.com>
2021-01-09 22:47:56 +0900

    <chore> change prod environment using amplify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   // 실제 배포 필요 13.125.238.130 임시로 설정
   production: true,
-  router: 'http://my.new.router.s3-website.ap-northeast-2.amazonaws.com',
+  router: 'https://master.dhn6ihoaa5lai.amplifyapp.com',
   userService: 'http://54.180.207.58:8080',
   appService: 'http://13.125.238.130:9090'
 };
