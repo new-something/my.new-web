7eb357d67d03068037670d4d67ae72a306a46940
ndgndg91 <ndgndg91@gmail.com>
2021-01-10 21:02:22 +0900

    <chore> change prod environment https domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   // 실제 배포 필요 13.125.238.130 임시로 설정
   production: true,
-  router: 'https://master.dhn6ihoaa5lai.amplifyapp.com',
+  router: 'https://my.new',
   userService: 'http://54.180.207.58:8080',
   appService: 'http://13.125.238.130:9090'
 };
