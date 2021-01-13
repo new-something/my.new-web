445cfb324397e155f4bdaa50c69aeed6db0b519d
ndgndg91 <ndgndg91@gmail.com>
2021-01-13 22:18:46 +0900

    <feat> sso

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,17 +1,29 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private jwtHelperService: JwtHelperService) { }
 
   isAuthenticated(): Observable<boolean> {
-    const session = localStorage.getItem('my-new-a');
+    const jwt = this.getCookieValue('my-new-a');
+    const expired = this.jwtHelperService.isTokenExpired(jwt);
+    if (!expired) {
+      localStorage.setItem('my-new-a', jwt);
+    }
+
     return new Observable<boolean>(subscriber => {
-      subscriber.next(!!session);
+      subscriber.next(!expired);
     });
+  }
+
+  getCookieValue(name): string {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) { return parts.pop().split(';').shift(); }
   }
 }
