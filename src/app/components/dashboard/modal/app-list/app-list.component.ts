ef9689863a12e84beff8f3e78633e27d191b09f1
ndgndg91 <ndgndg91@gmail.com>
2021-01-16 13:02:07 +0900

    <feat> app list modal tag change event

--- src/app/components/dashboard/modal/app-list/app-list.component.ts
+++ src/app/components/dashboard/modal/app-list/app-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-app-list',
@@ -6,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-list.component.css']
 })
 export class AppListComponent implements OnInit {
+  private appService: string = environment.appService;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -15,4 +18,26 @@ export class AppListComponent implements OnInit {
   hideAppListModal(): void {
     document.querySelector('#app-list-modal').classList.remove('is-active');
   }
+
+  changeAppTag($event): void {
+    console.log($event);
+    console.log($event.target);
+    console.log($event.target.value);
+    const url = this.appService + '/a/apps?tag=' + $event.target.value;
+    console.log(url);
+    this.httpClient.get<ProvidedAppResponse>(url).subscribe(data => {
+      console.log(data);
+    });
+  }
+}
+
+interface ProvidedAppResponse {
+  appCode: number;
+  appName: string;
+  appIcon: string;
+  domain: string;
+  description: string;
+
+  connected: boolean;
+  connectedId: number;
 }
