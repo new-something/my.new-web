7ca0de3dd22fa903dd0a0e481449a79b25deefe8
ndgndg91 <ndgndg91@gmail.com>
2021-01-20 22:17:40 +0900

    <refactor> my info interface

--- src/app/services/my-info.service.ts
+++ src/app/services/my-info.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {MyInfo} from '../models/my-info';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {map} from 'rxjs/operators';
+import {ConnectedApp} from '../models/connected-app';
+import {Shortcut} from '../models/shortcut';
+import {UrlRedirection} from '../models/url-redirection';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +18,46 @@ export class MyInfoService {
   constructor(private httpClient: HttpClient) { }
 
   myInfo(): Observable<MyInfo> {
-    return this.httpClient.get<MyInfo>(this.myInfoEndPoint).pipe();
+    return this.httpClient.get<MyInfoResponse>(this.myInfoEndPoint).pipe(
+      map(resp =>
+        new MyInfo(
+          resp.connectedApps.map(c => new ConnectedApp(c.connectedId, c.appCode, c.appName, c.appIcon, c.domain, c.description)),
+          resp.shortcuts.map(s => new Shortcut(s.shortcutId, s.path, s.description, s.type, s.description, s.appIcon, s.connectedId)),
+          resp.urlRedirections.map(u => new UrlRedirection(u.urlRedirectionId, u.path, u.destinationUrl))
+        )
+      )
+    );
   }
+}
+
+interface MyInfoResponse {
+  connectedApps: ConnectedAppResponse[];
+  shortcuts: ShortcutResponse[];
+  urlRedirections: UrlRedirectionResponse[];
+}
+
+interface ConnectedAppResponse {
+  connectedId: number;
+  appCode: number;
+  appName: string;
+  appIcon: string;
+  domain: string;
+  description: string;
+}
+
+interface ShortcutResponse {
+  shortcutId: number;
+  path: string;
+  destinationUrl: string;
+  type: string;
+  description: string;
+  appIcon: string;
+
+  connectedId: number;
+}
+
+interface UrlRedirectionResponse {
+  urlRedirectionId: number;
+  path: string;
+  destinationUrl: string;
 }
